dc5e3cbc9c0874cfb7208254e2e8dbc499fe87b0
mlafuente <mlafuente@revmaster.com>
2023-11-13 15:06:29 -0800

    working with slider

--- public/js/slide.js
+++ public/js/slide.js
@@ -14,20 +14,35 @@ function showSlide() {
 
 	//poner active al primer slide
 	slides[0].classList.remove('inactive');
+	slides[0].classList.add('active');
 
-	let moveInTimeout = setTimeout(() => {
-		if (slides.length > 1) {
-			slides[1].classList.remove('inactive');
-			slides[1].classList.add('active');
-			slides[0].classList.add('move-out');
-		}
-	}, 2000);
+	//Slide Start
+	//Slide move by time
+	//Slide move by click
+	let moveInTimeout = setTimeout(
+		slides => {
+			if (slides.length > 1) {
+				slides[1].classList.remove('inactive');
+				slides[1].classList.add('active');
+				slides[0].classList.remove('active');
+				slides[0].classList.add('move-out');
+			}
+			removeLastSlide(slides[0]);
+		},
+		3000,
+		slides
+	);
 
 	setTimeout(() => {
 		clearTimeout(moveInTimeout);
 	}, 3000);
 }
-
+function removeLastSlide(slide) {
+	setTimeout(() => {
+		slide.classList.remove('move-out');
+		slide.classList.add('inactive');
+	}, 3000);
+}
 function changeSlide(n) {
 	console.log(n);
 	// clearTimeout(autoSlideTimeout);
